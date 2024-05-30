2902e670278671eed5bd9ef40a573f9dfa7e73d3
Ainkharan <ainkharansoftdev@gmail.com>
2024-05-30 12:12:17 +0100

    All Task Completed Successfully

--- demospringbootapp/demospringbootapp/src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
+++ demospringbootapp/demospringbootapp/src/main/java/com/northcoders/demospringbootapp/controller/DemoController.java
@@ -1,8 +1,13 @@
 package com.northcoders.demospringbootapp.controller;
 
+
+import com.northcoders.demospringbootapp.model.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
@@ -11,6 +16,17 @@ public class DemoController {
     @GetMapping("/hello")
     public String getGreeting(){
         return "Hello There First Spring boot project, well exited !";
+    }
+
+    @GetMapping("/person")
+    public List<Person> getPerson(){
+
+        List<Person> personList = Arrays.asList(
+                new Person("Nada", 22, "absd@.com", "London", "Pizza"),
+                new Person("Bill", 43, "email@.com", "Brazil", "Cod Fish")
+        );
+        return personList;
+
     }
 
 
